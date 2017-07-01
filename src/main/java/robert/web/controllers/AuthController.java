dbfd756917ad33a52e9763c3f90563d3720e9595
robert <nowak021@gmail.com>
2017-07-01 23:04:51 +0200

    .

--- src/main/java/robert/web/controllers/AuthController.java
+++ src/main/java/robert/web/controllers/AuthController.java
@@ -1,30 +1,13 @@
 package robert.web.controllers;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthController {
 
-	private static final String HOME_PAGE = "redirect:/index.html";
-
 	@GetMapping("/")
 	public String homePage() {
-		return HOME_PAGE;
-	}
-
-	@GetMapping(value = "/logout") //TODO
-	public String logout(HttpServletRequest request) {
-		SecurityContextHolder.clearContext();
-		HttpSession session = request.getSession(false);
-		if ( session != null ) {
-			session.invalidate();
-		}
-
-		return HOME_PAGE;
+		return "redirect:/index.html";
 	}
 }
